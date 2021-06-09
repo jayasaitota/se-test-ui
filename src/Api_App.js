339907405f972eb23d0358d5e672b6b774d93c13
jayasaitota <jayasaitota@gmail.com>
2021-06-09 11:00:43 +0530

    Adde new app js to load data from development server appi
    Adde new app js to load data from development server appi

---
+++ src/Api_App.js
@@ -0,0 +1,72 @@
+import React ,{Component} from 'react';
+import ServerApi from './ServerApi';
+
+import SearchBar from './components/SearchBar';
+import ArticleDetails from './components/ArticleDetails';
+import Loader from './components/Loader';
+
+class App extends Component{
+  state = {
+    newsArticles : [], 
+    filteredArticles:[],
+    showFilterData:false,
+    showNoRecordsMsg:false
+  };
+
+  componentDidMount(){
+    const getTopArticles = async()=>{
+      let {data} = await ServerApi.get('/top-headlines',{})
+      this.setState({
+        newsArticles:data.articles,
+        showNoRecordsMsg:!data.articles.length?true:false
+      })
+    }
+    getTopArticles()
+  }
+
+  // Serach for the articles with keyword on users submit action
+  onSearchSubmit =async (topic)=>{
+    let {data} = await ServerApi.get('/everything',{
+       params:{
+        q:topic,
+       }
+     })
+     this.setState({
+       newsArticles:data.articles,
+       showNoRecordsMsg:!data.articles.length?true:false
+     })
+   };
+
+   //Filter the articles title with searchKeywords
+   filterArticles =async (e)=>{
+      if(!e){
+       await this.setState({
+        showFilterData:false,
+          filteredArticles:[]
+        })
+      }else{
+        await this.setState({
+          showFilterData:true,
+          filteredArticles:this.state.newsArticles.filter((item)=>item.title.toLowerCase().includes(e.toLowerCase()))
+        })
+      }
+   }
+
+  render(){
+    return (
+      <div className="ui container" style={{marginTop:"10px"}}>
+        <SearchBar onSubmit={this.onSearchSubmit} onSearchTextChange= {this.filterArticles} />
+        <div className='ui items'>
+          <ArticleDetails articles = {this.state.showFilterData?this.state.filteredArticles:this.state.newsArticles} />
+        </div>
+          {
+            this.state.showNoRecordsMsg?<h3>No records Found</h3>:
+            !this.state.newsArticles.length?
+            <Loader />:null
+          }
+      </div>
+    );
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import ApiApp from './Api_App';
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// Un comment this and comment App to use server api instead news api
+// ReactDOM.render(
+//   <ApiApp />,
+//   document.getElementById('root')
+// );
